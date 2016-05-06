a8f606113c6f16d7859bcc23baaeef83abaf421c
Explorer09 <explorer09@gmail.com>
2016-05-06 10:29:24 +0800

    TasksMeter behavior changes
    1. Enable non-percent graph display.
    2. Kernel threads are now always drawn in bar and graph modes,
       regardless of htop's hideKernelThreads option.
    3. Supplemental text in bar mode now shows "running tasks / total
       tasks". Previous behavior was "running tasks / largest total tasks
       ever recorded during lifetime of the meter".

--- TasksMeter.c
+++ TasksMeter.c
@@ -24,13 +24,7 @@ static void TasksMeter_updateValues(Meter* this, char* buffer, int len) {
    this->values[1] = pl->userlandThreads;
    this->values[2] = pl->totalTasks - pl->kernelThreads - pl->userlandThreads;
    this->values[3] = pl->runningTasks;
-   if (pl->totalTasks > this->full) {
-      this->full = pl->totalTasks;
-   }
-   if (this->pl->settings->hideKernelThreads) {
-      this->values[0] = 0;
-   }
-   snprintf(buffer, len, "%d/%d", (int) this->values[3], (int) this->full);
+   snprintf(buffer, len, "%d/%d", pl->runningTasks, pl->totalTasks);
 }
 
 static void TasksMeter_display(Object* cast, RichString* out) {
@@ -75,7 +69,7 @@ MeterClass TasksMeter_class = {
    .updateValues = TasksMeter_updateValues,
    .defaultMode = TEXT_METERMODE,
    .maxItems = 4,
-   .full = 100.0,
+   .full = -1.0,
    .attributes = TasksMeter_attributes, 
    .name = "Tasks",
    .uiName = "Task counter",
